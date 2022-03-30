72716127f7aebe2ce97fa811d4dde30abe9dbadf
Robert Scott-Buccleuch <60840+rsb@users.noreply.github.com>
2022-03-30 15:56:33 -0700

    finish tutorial

--- program/src/error.rs
+++ program/src/error.rs
@@ -8,6 +8,14 @@ pub enum EscrowError {
 
   #[error("Not Rent Exempt")]
   NotRentExempt,
+
+  /// Expected Amount Mismatch
+  #[error("Expected Amount Mismatch")]
+  ExpectedAmountMismatch,
+
+  /// Amount Overflow
+  #[error("Amount Overflow")]
+  AmountOverflow,
 }
 
 impl From<EscrowError> for ProgramError {
